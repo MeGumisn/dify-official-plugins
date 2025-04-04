03536b4a56b051e27e6164fe28f29e5b3b2218a0
MeGumisn <tomhe@bu.edu>
2025-04-04 11:15:24 +0800

    [ADD]为deepseek-r1添加联网搜索（夸克）

--- models/tongyi/models/llm/llm.py
+++ models/tongyi/models/llm/llm.py
@@ -668,6 +668,48 @@ class TongyiLargeLanguageModel(LargeLanguageModel):
         :param credentials: model credentials
         :return: AIModelEntity or None
         """
+        rules = [
+            ParameterRule(
+                name="temperature",
+                use_template="temperature",
+                label=I18nObject(en_US="Temperature", zh_Hans="温度"),
+                type=ParameterType.FLOAT,
+            ),
+            ParameterRule(
+                name="max_tokens",
+                use_template="max_tokens",
+                default=512,
+                min=1,
+                max=int(credentials.get("max_tokens", 1024)),
+                label=I18nObject(en_US="Max Tokens", zh_Hans="最大标记"),
+                type=ParameterType.INT,
+            ),
+            ParameterRule(
+                name="top_p",
+                use_template="top_p",
+                label=I18nObject(en_US="Top P", zh_Hans="Top P"),
+                type=ParameterType.FLOAT,
+            ),
+            ParameterRule(
+                name="top_k",
+                use_template="top_k",
+                label=I18nObject(en_US="Top K", zh_Hans="Top K"),
+                type=ParameterType.FLOAT,
+            ),
+            ParameterRule(
+                name="frequency_penalty",
+                use_template="frequency_penalty",
+                label=I18nObject(en_US="Frequency Penalty", zh_Hans="重复惩罚"),
+                type=ParameterType.FLOAT,
+            ),
+        ]
+        if "deepseek-r1" in model:
+            rules.append(ParameterRule(
+                name="enable_search",
+                use_template="enable_search",
+                label=I18nObject(en_US="Enable Search(Quark)", zh_Hans="启用联网搜索（夸克）"),
+                type=ParameterType.BOOLEAN,
+            ))
         return AIModelEntity(
             model=model,
             label=I18nObject(en_US=model, zh_Hans=model),
@@ -688,39 +730,5 @@ class TongyiLargeLanguageModel(LargeLanguageModel):
                 ),
                 ModelPropertyKey.MODE: LLMMode.CHAT.value,
             },
-            parameter_rules=[
-                ParameterRule(
-                    name="temperature",
-                    use_template="temperature",
-                    label=I18nObject(en_US="Temperature", zh_Hans="温度"),
-                    type=ParameterType.FLOAT,
-                ),
-                ParameterRule(
-                    name="max_tokens",
-                    use_template="max_tokens",
-                    default=512,
-                    min=1,
-                    max=int(credentials.get("max_tokens", 1024)),
-                    label=I18nObject(en_US="Max Tokens", zh_Hans="最大标记"),
-                    type=ParameterType.INT,
-                ),
-                ParameterRule(
-                    name="top_p",
-                    use_template="top_p",
-                    label=I18nObject(en_US="Top P", zh_Hans="Top P"),
-                    type=ParameterType.FLOAT,
-                ),
-                ParameterRule(
-                    name="top_k",
-                    use_template="top_k",
-                    label=I18nObject(en_US="Top K", zh_Hans="Top K"),
-                    type=ParameterType.FLOAT,
-                ),
-                ParameterRule(
-                    name="frequency_penalty",
-                    use_template="frequency_penalty",
-                    label=I18nObject(en_US="Frequency Penalty", zh_Hans="重复惩罚"),
-                    type=ParameterType.FLOAT,
-                ),
-            ],
+            parameter_rules=rules,
         )
